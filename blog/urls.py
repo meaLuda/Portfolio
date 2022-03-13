0a5d0127382518ef736da5960bac72126e711a92
meaLuda <munyalamea@gmail.com>
2022-03-13 17:37:09 +0300

    deployment 6

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from .views import blog,PostDetail
 from django.urls import path
 
 urlpatterns = [
-    path('home/', blog.as_view(), name='home'),
+    path('', blog.as_view(),name='blog'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
